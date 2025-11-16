0398fdcf95230c73fa9c88ff499e95a7e1dae43f
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-16 20:44:05 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Introduzca un número: "));
+
+        int aux = numeroIntroducido;
+        int volteado = 0;
+        int longitud = 0;
+        int sumaDigitos = 0;
+
+        while (aux != 0) {
+            volteado = aux % 10;
+            sumaDigitos += volteado;
+            aux /= 10;
+            longitud++;
+        }
+
+        double media = (double)sumaDigitos/longitud;
+
+        System.out.printf("La media de sus dígitos es %.2f", media);
     }
 }
